817b853e0b3879fd5e5d7db975c76611e69aff05
Islam Ramdan <121763270+IslamRamdan@users.noreply.github.com>
2026-03-13 15:45:26 +0200

    logo tab

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
+    <link rel="icon" type="image/png" href="{{ asset('image/logo-light.png') }}">
     <style>
         :root {
             --primary: #6366f1;
